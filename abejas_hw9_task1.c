943acbe881d06642e8725f29c1549dfacc81bc7b
ahlweb <adamlee1@mail.weber.edu>
2017-04-08 05:34:10 +0000

    ReadFile func. done

--- abejas_hw9_task1.c
+++ abejas_hw9_task1.c
@@ -13,16 +13,49 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+
+#define SIZE 81
+	
+
+
 /* Function Prototypes */
 
+
+void ReadFile(FILE *file1, float num[]);
+	float data[SIZE];
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	FILE *infile;
+	infile = fopen(argv[1],"r");
+	
+	if(infile == NULL)
+	{
+		printf("The file was not successfully read.\n");
+		exit(1);
+	}
+	printf("The dat file %s was successfully read.\n\n", argv[1]);
 
+	ReadFile(infile, data);
+
+
+	
+	fclose(infile);
 	return 0;
 }
 
 
 /* Function Defenitions */
 
+void ReadFile(FILE *file1, float num[])
+{
+	int count = 0;
+	while(fscanf(file1, "%f" , &num[count]) != EOF)
+	{
+		printf("scanning %f\n", num[count]);
+	count++;
+	}
+
+	return;
+}
 
