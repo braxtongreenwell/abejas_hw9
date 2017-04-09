de7429e03a474bf25e0928b9775c9f8c20e7ff87
braxtongreenwell <braxtongreenwell@mail.weber.edu>
2017-04-09 04:29:45 +0000

    committing to pull

--- abejas_hw9_task1.c
+++ abejas_hw9_task1.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  abejas_hw9_task1.c
  *       	Usage:  ./abejas_hw9_task1.c
- *    Description:  
+ *    Description:  homework for task1
  *
  *        Version:  1.0
  *        Created:  04/04/2017 04:37:38 PM
@@ -14,15 +14,23 @@
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
 /* Function Prototypes */
-
+void Usage(void);
 /* Main Program */
 int main(int argc, char *argv[])
 {
-
+	if(argc != 3)
+	{
+		Usage()
+	}
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(void)
+{
+
+	return;
+}
 
 
